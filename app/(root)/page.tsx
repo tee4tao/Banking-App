7a6ac6a23b2e3c09963d3a20c694b0234f942e9c
Taofeek Adeniyi <adeniyitaofeek001@gmail.com>
2024-09-17 17:57:33 +0100

    work on header dashboard and sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const loggedIn = { firstName: "Taofeek" };
+  return (
+    <section>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions effectively"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
